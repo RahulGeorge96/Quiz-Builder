547c5275e4d18cd4dfca9c167655599e30da9b7a
Rahul George <rahuljithu911@gmail.com>
2024-11-11 23:18:25 +0530

    added

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://boisterous-tulumba-8058b2.netlify.app/",
+      "https://boisterous-tulumba-8058b2.netlify.app",
     ],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
